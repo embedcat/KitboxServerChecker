4f75e7ed0489f92890175387a68dae4167ef62bd
kip <kip@kit-invest.ru>
2022-02-22 12:50:35 +0300

    добавил интерактивность

--- main.py
+++ main.py
@@ -1,26 +1,77 @@
+import json
+import logging
+import os
+import threading
 import time
 import requests
+import telebot
+from dotenv import load_dotenv
 
-# SERVER_URL = 'http://127.0.0.1:5000/api{num}'
-SERVER_URL = 'http://kitbox{num}.kit-invest.ru/KitBoxService.svc/SendRequest'
+
+load_dotenv()
+
+USERS_FILE = "users.json"
+# SERVER_URL = "http://127.0.0.1:5000/api{num}"
+SERVER_URL = "http://kitbox{num}.kit-invest.ru/KitBoxService.svc/SendRequest"
 
 TIME_POLL_SEC = 90
+TG_TOKEN = os.environ.get("TG_TOKEN")
+EMOJI_OK = "\U0001f7E2"
+EMOJI_FAIL = "\U0001f534"
 
-TG_TOKEN = '5154060556:AAFCeRYqle6fbNsh7Qa3EAPswK9i56NpVEc'
-TG_CHAT_IDS = ['376585847', '753407442', '281605899']
-# TG_CHAT_IDS = ['376585847']
+MSG_SERVER_NOT_ANSWER = "{emoji} Сервер не отвечает. Ошибка: {error}"
+MSG_SERVER_ERROR = "{emoji} Сервер не отвечает. Статус: {status}"
+MSG_SERVER_OK = "{emoji} Сервер работает! Отлично!"
+MSG_START = "Я буду присылать сообщения при изменении статуса сервера"
+MSG_STOP = "Больше не буду беспокоить. Для возобновления рассылки присылай /start"
+MSG_HELP = "/start - начать присылать уведомления о смене статуса сервера\n" \
+           "/stop - не присылать уведомления о смене статуса сервера\n" \
+           "/check - проверить сервер прямо сейчас\n" \
+           "/stat - вывести статистику сервера\n" \
+           "/help - показать эту справку"
 
-EMOJI_OK = '\U0001f7E2'
-EMOJI_FAIL = '\U0001f534'
+telebot.logger.setLevel(logging.INFO)
+bot = telebot.TeleBot(TG_TOKEN)
+
+users = {}
 
 
-def send_tg_msg(msg: str = '') -> None:
-    for recipient in TG_CHAT_IDS:
-        requests.post(
-            url=f'https://api.telegram.org/bot{TG_TOKEN}/sendMessage',
-            data={'chat_id': recipient, 'text': str(msg)}
-        ).json()
-        time.sleep(1)
+def update_users_json():
+    with open(USERS_FILE, "w") as file:
+        json.dump(users, file)
+
+
+@bot.message_handler(commands=["start"])
+def bot_start(message):
+    user = {message.chat.username: str(message.chat.id)}
+    users.update(user)
+    update_users_json()
+    bot.send_message(chat_id=message.chat.id, text=MSG_START)
+
+
+@bot.message_handler(commands=["stop"])
+def bot_stop(message):
+    users.pop(message.chat.username)
+    update_users_json()
+    bot.send_message(chat_id=message.chat.id, text=MSG_STOP)
+
+
+@bot.message_handler(commands=["help"])
+def bot_help(message):
+    bot.send_message(chat_id=message.chat.id, text=MSG_HELP)
+
+
+@bot.message_handler(commands=["check"])
+def bot_help(message):
+    result = check_server_ok()
+    msg = result_get_message(result=result)
+    bot.send_message(chat_id=message.chat.id, text=msg)
+
+
+def send_update_status(msg: str = "") -> None:
+    for username, chat_id in users.items():
+        bot.send_message(chat_id=chat_id, text=msg)
+        time.sleep(0.2)
 
 
 def check_server_ok() -> dict:
@@ -35,27 +86,39 @@ def check_server_ok() -> dict:
     return {"result": True}
 
 
-def main():
+def result_get_message(result: dict) -> str:
+    if result["result"] is False:
+        if result["status"] is None:
+            return MSG_SERVER_NOT_ANSWER.format(emoji=EMOJI_FAIL, error=result["error"])
+        else:
+            return MSG_SERVER_ERROR.format(emoji=EMOJI_FAIL, status=result["status"])
+    else:
+        return MSG_SERVER_OK.format(emoji=EMOJI_OK)
+
+
+def server_check_thread():
     is_first_iter = True
     server_status_ok = False
 
     while True:
         result = check_server_ok()
-        if (server_status_ok or is_first_iter) and result['result'] is False:
-            if result['status'] is None:
-                msg = f"{EMOJI_FAIL} Server is down. Server error {result['error']}"
-            else:
-                msg = f"{EMOJI_FAIL} Server is down. Status code {result['status']}"
-            send_tg_msg(msg=msg)
+        msg = result_get_message(result=result)
+        if (server_status_ok or is_first_iter) and result["result"] is False:
+            send_update_status(msg=msg)
             server_status_ok = False
-        elif (not server_status_ok or is_first_iter) and result['result'] is True:
-            msg = f'{EMOJI_OK} Server is OK. Awesome!'
-            send_tg_msg(msg=msg)
+        elif (not server_status_ok or is_first_iter) and result["result"] is True:
+            send_update_status(msg=msg)
             server_status_ok = True
 
         is_first_iter = False
         time.sleep(TIME_POLL_SEC)
 
 
-if __name__ == '__main__':
-    main()
+if __name__ == "__main__":
+    with open(USERS_FILE, "r") as file:
+        users = json.load(file)
+
+    t1 = threading.Thread(target=server_check_thread)
+    t1.start()
+    bot.infinity_polling()
+
